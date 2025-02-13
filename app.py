3e13f91a3df2b845ece5f5ea9580120b68bdb19b
Yusuf Khan <146713368+itsuf@users.noreply.github.com>
2025-02-13 21:22:58 +0000

    Update app.py
    updated main so it actually works with new init file

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from main import app # import the Flask app from main folder
+from main import create_app # Import flask app from main folder
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
