a6be3ebf72faeb8e9220bb6f53ab8096cc3a246e
ldarnbr <ldarnbr@gmail.com>
2025-03-22 13:55:58 +0000

    feat: Create auction page image upload changes
    Create auction page now renders a list of the
    image names below the file selector.
    
    When user uploads more than 5 images at a time,
    the file selector flushes all suspended files and
    alerts the user before submitting the form.

--- main/static/js/createAuction.js
+++ main/static/js/createAuction.js
@@ -12,3 +12,23 @@ $('#create-auction-form').on('submit', (e) => {
     }
   }
 })
+
+$(document).ready(function() {
+  $('#upload-images').on('change', function() {
+    console.log("Image upload changes");
+    let images = this.files;
+    let imageList = $('#image-list');
+    imageList.empty();
+
+    if (images.length > 5) {
+      // Error message when user uploads more than 5 images
+      window.alert("Please select up to 5 images maximum")
+      // Clear the uploaded files from the file selector
+      this.value = "";
+    } else if (images.length > 0) {
+      // Render a list of the filenames with each name in its own div
+      let imageNames = Array.from(images).map(image => `<div>${image.name}</div>`).join('\n');
+      imageList.append(imageNames);
+    }
+  });
+});
