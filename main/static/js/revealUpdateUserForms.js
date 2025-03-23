7f4584fbf464d899ecf2bf212e8bafc4df6c3747
ldarnbr <ldarnbr@gmail.com>
2025-03-23 19:39:23 +0000

    fix: fixed a bug where all forms were submitting data when updating one user info form

--- main/static/js/revealUpdateUserForms.js
+++ main/static/js/revealUpdateUserForms.js
@@ -16,6 +16,23 @@ $(document).ready(function() {
     
     // Highlights only the selected form link
     $('#' + formId + '-link').addClass('active');
+
+    // Keeps track of the form that the user used
+    localStorage.setItem('activeForm', formId);
+
+    // Flushes the data from the hidden forms
+    if (formId != 'username') {
+      $('#update-username-form')[0].reset();
+      $('#update-username-form .alert.alert-danger').remove();
+    }
+    if (formId != 'email') {
+        $('#update-email-form')[0].reset();
+        $('#update-email-form .alert.alert-danger').remove();
+    }
+    if (formId != 'password') {
+        $('#update-password-form')[0].reset();
+        $('#update-password-form .alert.alert-danger').remove();
+    }
   }
 
   // Attach click events to each list item
@@ -31,7 +48,8 @@ $(document).ready(function() {
     showForm('password');
   });
 
-  // Shows the username form initially
-  showForm('username');
+  // Render the form the user was last on, or username by default
+  let form = localStorage.getItem('activeForm') || 'username';
+  showForm(form)
 
 });
